408c590c0eeb48f8dd5a5b9b4b2da83477e8500e
kiwsan <kiwsanthia@gmail.com>
2019-07-20 11:54:32 +0700

    initial commit

---
+++ ContosoUniversity.UnitTests/UnitTest1.cs
@@ -0,0 +1,13 @@
+using System;
+using Xunit;
+
+namespace ContosoUniversity.UnitTests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+        }
+    }
+}
